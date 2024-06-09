b8656a755bd002a0d38127ca7d9d908ddc10df5a
alyshim <alyona.shimchuk@gmail.com>
2024-06-09 17:23:23 +0200

    Fixing bugs

--- src/index.js
+++ src/index.js
@@ -40,7 +40,7 @@ function formatDate(date) {
 }
 
 function searchCity(city) {
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "33ftabad4a7f60213o971b6848003fd4";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(refreshWeather);
 }
